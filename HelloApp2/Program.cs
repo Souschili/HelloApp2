893645d07050ec1467f9277de5f2b7263e576c7b
Sous Chili <greatdragone75@gmail.com>
2019-01-07 23:01:19 +0400

    Add project files.

---
+++ HelloApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
